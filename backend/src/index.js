912770a4069a45815afe5ebe94d1de94f911e85e
ivan <thanhtkcb2004@gmail.com>
2026-06-03 16:27:09 +0700

    test ci

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ connectDB(); // Gọi hàm kết nối DB
 const allowedOrigins = [
     process.env.CORS_ORIGIN,
     "http://localhost:3000",
-    "http://localhost:5173",
+    "http://localhost:4173",
 ];
 
 app.use(cors({
@@ -61,7 +61,7 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);   
 app.use("/api/applications", applicationRoutes);
 app.use("/api/candidates", candidateRoutes);
 app.use("/api/companies", companyRoutes);
